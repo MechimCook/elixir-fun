6400088d71e743cef74e860b9cd8c71cf084a75b
Mechim <mechim.cook20@gmail.com>
2020-06-23 15:59:55 -0400

    Flatten array basic

---
+++ flatten-array/lib/flatten_array.ex
@@ -0,0 +1,17 @@
+defmodule FlattenArray do
+  @doc """
+    Accept a list and return the list flattened without nil values.
+
+    ## Examples
+
+      iex> FlattenArray.flatten([1, [2], 3, nil])
+      [1,2,3]
+
+      iex> FlattenArray.flatten([nil, nil])
+      []
+
+  """
+
+  @spec flatten(list) :: list
+  def flatten(list), do: List.flatten(list) |> Enum.filter(& &1)
+end
