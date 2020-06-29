958306373fb0e033bf746eecdaa239faf0daaaac
Mechim <mechim.cook20@gmail.com>
2020-06-29 14:00:28 -0400

    Bank account V1

---
+++ bank-account/lib/bank_account.ex
@@ -0,0 +1,50 @@
+defmodule BankAccount do
+  @moduledoc """
+  A bank account that supports access from multiple processes.
+  """
+
+  @typedoc """
+  An account handle.
+  """
+
+  @opaque account :: pid
+
+  @doc """
+  Open the bank. Makes the account available.
+  """
+  @spec open_bank() :: account
+  def open_bank() do
+    {:ok, account} = Agent.start_link(fn -> 0 end)
+    account
+  end
+
+  @doc """
+  Close the bank. Makes the account unavailable.
+  """
+  @spec close_bank(account) :: none
+  def close_bank(account), do: Agent.stop(account)
+
+  @doc """
+  Get the account's balance.
+  """
+  @spec balance(account) :: integer
+  def balance(account) do
+    if Process.alive?(account) do
+      Agent.get(account, fn balance -> balance end)
+    else
+      {:error, :account_closed}
+    end
+  end
+
+  @doc """
+  Update the account's balance by adding the given amount which may be negative.
+  """
+  @spec update(account, integer) :: any
+  def update(account, amount) do
+    if Process.alive?(account) do
+      Agent.update(account, fn balance -> balance + amount end)
+    else
+      {:error, :account_closed}
+    end
+  end
+end
