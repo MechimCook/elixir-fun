28a822f3028966d9e987743d393512cb324353cb
Mechim <mechim.cook20@gmail.com>
2020-08-05 11:54:22 -0400

    Word cound V2

--- word-count/lib/word_count.ex
+++ word-count/lib/word_count.ex
@@ -4,13 +4,29 @@ defmodule WordCount do
 
   Words are compared case-insensitively.
   """
+  @word_dividers ["!", "&", "$", "%", "^", "@", ":", "_", ",", " "]
   @spec count(String.t()) :: map
   def count(sentence) do
     sentence
     |> String.downcase()
-    |> (&Regex.scan(~r/(([[:alnum:]-]){1,})/u, &1, capture: :first)).()
-    |> Enum.reduce(%{}, fn [key], acc ->
-      Map.update(acc, key, 1, &(&1 + 1))
-    end)
+    |> String.split(@word_dividers, trim: true)
+    |> Enum.reduce(%{}, fn word, acc -> Map.update(acc, word, 1, &(&1 + 1)) end)
   end
 end
+
+# defmodule WordCount do
+#   @doc """
+#   Count the number of words in the sentence.
+#
+#   Words are compared case-insensitively.
+#   """
+#   @spec count(String.t()) :: map
+#   def count(sentence) do
+#     sentence
+#     |> String.downcase()
+#     |> (&Regex.scan(~r/(([[:alnum:]-]){1,})/u, &1, capture: :first)).()
+#     |> Enum.reduce(%{}, fn [key], acc ->
+#       Map.update(acc, key, 1, &(&1 + 1))
+#     end)
+#   end
+# end
