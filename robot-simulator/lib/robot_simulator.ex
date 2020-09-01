cb1a3721bf12df6aa9c3aa140c591726c6bd7c51
Mechim <mechim.cook20@gmail.com>
2020-09-01 12:17:30 -0400

    Robot sim fix

--- robot-simulator/lib/robot_simulator.ex
+++ robot-simulator/lib/robot_simulator.ex
@@ -1,5 +1,6 @@
 defmodule RobotSimulator do
   @directions [:north, :east, :south, :west]
+  @turns [{:north, :east}, {:east, :south}, {:south, :west}, {:west, :north}]
 
   defguard is_direction?(direction) when is_atom(direction) and direction in @directions
   defguard valid_position?(x, y) when is_integer(x) and is_integer(y)
@@ -10,11 +11,13 @@ defmodule RobotSimulator do
   Valid directions are: `:north`, `:east`, `:south`, `:west`
   """
   @spec create(direction :: atom, position :: {integer, integer}) :: any
-  def create(direction \\ :north, {x, y} \\ {0, 0})
-      when is_direction(direction) and valid_position?(x, y),
+  def create(direction \\ :north, position \\ {0, 0})
+
+  def create(direction, {x, y})
+      when is_direction?(direction) and valid_position?(x, y),
       do: {direction, {x, y}}
 
-  def create(direction, _position) when is_direction(direction), do: {:error, "invalid position"}
+  def create(direction, _position) when is_direction?(direction), do: {:error, "invalid position"}
   def create(_direction, _position), do: {:error, "invalid direction"}
 
   @doc """
@@ -23,27 +26,12 @@ defmodule RobotSimulator do
   Valid instructions are: "R" (turn right), "L", (turn left), and "A" (advance)
   """
   @spec simulate(robot :: any, instructions :: String.t()) :: any
-  def simulate(robot, instructions) do
-    String.codepoints(instructions)
-    |> Enum.reduce_while(
-      robot,
-      fn instruction, robot ->
-        case instruct(instruction, robot) do
-          :error ->
-            {:halt, {:error, "invalid instruction"}}
-
-          new_robot ->
-            {:cont, new_robot}
-        end
-      end
-    )
-  end
 
   def simulate({direction, position}, "L" <> tail),
-    do: simulate({@left[direction], position}, tail)
+    do: simulate({left(direction), position}, tail)
 
   def simulate({direction, position}, "R" <> tail),
-    do: simulate({@right[direction], position}, tail)
+    do: simulate({right(direction), position}, tail)
 
   def simulate({:north, {x, y}}, "A" <> tail), do: simulate({:north, {x, y + 1}}, tail)
   def simulate({:east, {x, y}}, "A" <> tail), do: simulate({:east, {x + 1, y}}, tail)
@@ -51,6 +39,16 @@ defmodule RobotSimulator do
   def simulate({:west, {x, y}}, "A" <> tail), do: simulate({:west, {x - 1, y}}, tail)
   def simulate(robot, ""), do: robot
   def simulate(_, _), do: {:error, "invalid instruction"}
+
+  defp left(direction) do
+    {new, _current} = Enum.find(@turns, fn {_new, current} -> current == direction end)
+    new
+  end
+
+  defp right(direction) do
+    {_current, new} = Enum.find(@turns, fn {current, _new} -> current == direction end)
+    new
+  end
 
   @doc """
   Return the robot's direction.
